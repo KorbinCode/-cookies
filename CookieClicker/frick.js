c15ff3614ba51d5dcc066bdb212178d40f956c2c
27MAK000 <78812907+27MAK000@users.noreply.github.com>
2021-09-21 12:38:58 -0500

    Update and rename CookieCrumbled.js to frick.js

---
+++ CookieClicker/frick.js
@@ -0,0 +1,15 @@
+Game.Win('Third-party');
+
+if(Frick === undefined) var Frick = {};
+Frick.name = 'Cookie Crumbled';
+Frick.version = '1.0';
+Frick.GameVersion = '2.022';
+Frick.modURL = 'https://klattmose.github.io/CookieClicker/CookieCrumbled.js';
+Frick.names = ['frick']
+while (true) {
+    if (Game.customGrandmaNames !== undefined) {            
+        break;
+    } 
+}
+
+Game.customGrandmaNames = Frick.names
