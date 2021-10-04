c4ba34b7230cfe31effab621595ece80aa95362a
27MAK000 <78812907+27MAK000@users.noreply.github.com>
2021-10-04 12:45:14 -0500

    Update frick.js

--- CookieClicker/frick.js
+++ CookieClicker/frick.js
@@ -1,1 +1,29 @@
-Game.Loader.Replace('perfectCookie.png', 'https://upload.wikimedia.org/wikipedia/en/c/c2/Peter_Griffin.png')
+
+const readyCheck = setInterval(() => {
+    const Game = unsafeWindow.Game;
+ 
+    if (
+        typeof Game !== "undefined" &&
+        typeof Game.ready !== "undefined" &&
+        Game.ready
+    ) {var A = "bgBlue.jpg"
+       var B = "grandmas1.jpg"
+       var C = "grandmas2.jpg"
+       var D = "grandmas3.jpg"
+       var X = "https://i.pinimg.com/originals/67/02/32/670232f4144343e71cbe7b11b7f4e232.jpg"
+if (Game.Loader.assetsLoaded.indexOf(A) == -1)
+	Game.Loader.Load([A]);
+Game.Loader.Replace(A, X);
+if (Game.Loader.assetsLoaded.indexOf(B) == -1)
+	Game.Loader.Load([B]);
+Game.Loader.Replace(B, X);
+if (Game.Loader.assetsLoaded.indexOf(C) == -1)
+	Game.Loader.Load([C]);
+Game.Loader.Replace(C, X);
+if (Game.Loader.assetsLoaded.indexOf(D) == -1)
+	Game.Loader.Load([D]);
+Game.Loader.Replace(D, X);
+        clearInterval(readyCheck);
+    }
+}, 1000);
+ 
