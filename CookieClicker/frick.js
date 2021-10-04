fa9845c01cd820e34d88a6e17b7d41490e863596
27MAK000 <78812907+27MAK000@users.noreply.github.com>
2021-10-04 12:30:55 -0500

    Update frick.js

--- CookieClicker/frick.js
+++ CookieClicker/frick.js
@@ -1,9 +1,1 @@
-Game.customGrandmaNames = ['frick']
-Game.customGrandmaNames = ['frick']
-Game.customGrandmaNames = ['frick']Game.customGrandmaNames = ['frick']
-Game.customGrandmaNames = ['frick']Game.customGrandmaNames = ['frick']
-Game.customGrandmaNames = ['frick']v
-
-Game.customGrandmaNames = ['frick']
-Game.customGrandmaNames = ['frick']kwjtgerhdfc.hbejrnhg,y.k;rtv
-'y5\]rthv
+Game.Loader.Replace('perfectCookie.png', 'https://upload.wikimedia.org/wikipedia/en/c/c2/Peter_Griffin.png')
